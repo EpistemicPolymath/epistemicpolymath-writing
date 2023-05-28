b2d690751ce418b65789ab4a9a157854c0374469
Jenn <epistemicpolymath@gmail.com>
2023-05-27 21:15:16 -0400

    Updated Google Analytics Tag Information Externally

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -21,6 +21,7 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports plausible, simpleAnalytics, umami or googleAnalytics
+    // Created a Google Analytics Tag for the Website
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
