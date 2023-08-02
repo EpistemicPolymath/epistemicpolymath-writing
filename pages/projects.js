d9f6891b09a0f5d6f0d5702f0479be3b768a77e2
Jenn <epistemicpolymath@gmail.com>
2023-08-02 11:45:00 -0400

    Updated Project page text.

--- pages/projects.js
+++ pages/projects.js
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            View what other projects I am working on!
           </p>
         </div>
         <div className="container py-12">
