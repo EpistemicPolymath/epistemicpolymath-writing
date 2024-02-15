6d02b51f7b1e69e94d5e636fc11372e26aa84bb0
EpistemicPolymath <epistemicpolymath@gmail.com>
2024-02-14 21:16:26 -0500

    Setup disqus for commenting

--- next.config.js
+++ next.config.js
@@ -7,13 +7,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app disqus.com *.google.com youtu.be youtube.com analytics.umami.is;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src giscus.app
+  frame-src giscus.app disqus.com youtube.com *.google.com youtu.be
 `
 
 const securityHeaders = [
